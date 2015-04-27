2bbba3ba94dfbd0aff9c6ed6748c3b4aa1cccb00
Frank Dai <frank821204@gmail.com>
2015-04-27 15:22:10 +0800

    add play list function

--- asset/app.js
+++ asset/app.js
@@ -1,5 +1,4 @@
 var $player=$('#player');
-var songs;
 $.getJSON('/getsongs',function(data){
 	var Song=Backbone.Model.extend({
 		play:function(){
@@ -12,7 +11,6 @@ $.getJSON('/getsongs',function(data){
 	});
 	var lib=new Library;
 	lib.add(data);
-	songs=lib;
 	var SongView = Backbone.View.extend({
 		tagName: "li",
 		className:'list',
@@ -20,7 +18,8 @@ $.getJSON('/getsongs',function(data){
 	  		'click':'play'
 	  	},
 	  	play:function(){
-	  		this.model.play();
+	  		var number=lib.models.indexOf(this.model);
+	  		currentSong=new CurrentSong({current:number});
 	  	},
 	  	template: _.template("Artist: <%= artist %>,Title:<%= title %>,Ablum:<%= album %>"),
 	  	render: function() {
@@ -28,6 +27,61 @@ $.getJSON('/getsongs',function(data){
 	    	return this;
 	  	}
 	}); 
+	var CurrentSong=Backbone.Model.extend({
+		defaults:{
+			current:0,
+			random:false,
+			repeat:true,
+			singleRepeat:false,
+		},
+		initialize:function(){
+			lib.models[this.attributes.current].play();
+		},
+		prev:function(){
+			var current=this.attributes.current;
+			var target=current-1;
+			if (target<0&&this.attributes.repeat){
+				target=lib.length-1;
+			}
+			if (this.attributes.singleRepeat) {
+				target=current;
+			}
+			return target;
+		},
+		next:function(){
+			var current=this.attributes.current;
+			var target=current+1;
+			if (target>=lib.length&&this.attributes.repeat){
+				target=0
+			}
+			if (this.attributes.singleRepeat) {
+				target=current;
+			}
+			return target;
+		},
+	});
+	var currentSong=new CurrentSong();
+	$('#play .next').on('click',function(){
+		var number=currentSong.next();
+		currentSong=new CurrentSong({current:number});
+	});
+	$('#play .prev').on('click',function(){
+		var number=currentSong.prev();
+		currentSong=new CurrentSong({current:number});
+	});
+	$('#play .play').on('click',function(){
+		var $this=$(this);
+		$this.toggleClass('paused');
+		if ($this.hasClass('paused')){
+			$player[0].pause();
+		} else {
+			$player[0].play();
+		}
+	});
+	$player.on('ended',function(event){
+		var number=currentSong.next();
+		currentSong=new CurrentSong({current:number});
+	});
 	var PlayList=Backbone.View.extend({
 		tagName:'ul',
 		className:'playlist',
@@ -42,7 +96,6 @@ $.getJSON('/getsongs',function(data){
 			});
 			return this;
 		}
-
 	});
 	var playList=new PlayList({collection:lib});
 });
