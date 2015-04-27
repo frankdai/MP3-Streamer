21a7b5c4cdc03fc9304614ecc0df0693a6d235e3
Frank Dai <frank821204@gmail.com>
2015-04-27 22:52:13 +0800

    update song playing info

--- asset/app.js
+++ asset/app.js
@@ -1,16 +1,25 @@
-var $player=$('#player');
+var streamer={};
 $.getJSON('/getsongs',function(data){
+	var player=document.getElementById('player');
+	//Song,Library MVC
 	var Song=Backbone.Model.extend({
 		play:function(){
-			$player.attr('src',this.attributes.file);
-			$player[0].play();
+			player.src=this.get('file');
+			player.play();
 		},
+		updateInfo:function(){
+			var artist=$('#song-info .artist');
+			var title=$('#song-info .title');
+			var album=$('#song-info .album');
+			artist.text(this.get('artist'));
+			title.text(this.get('title'));
+			album.text(this.get('album'));
+		}
 	});
 	var Library = Backbone.Collection.extend({
   		model: Song,
 	});
-	var lib=new Library;
-	lib.add(data);
+	var lib=new Library(data);
 	var SongView = Backbone.View.extend({
 		tagName: "li",
 		className:'list',
@@ -18,8 +27,8 @@ $.getJSON('/getsongs',function(data){
 	  		'click':'play'
 	  	},
 	  	play:function(){
-	  		var number=lib.models.indexOf(this.model);
-	  		currentSong=new CurrentSong({current:number});
+	  		var number=lib.indexOf(this.model);
+	  		currentSong.set('current',number);
 	  	},
 	  	template: _.template("Artist: <%= artist %>,Title:<%= title %>,Ablum:<%= album %>"),
 	  	render: function() {
@@ -27,61 +36,6 @@ $.getJSON('/getsongs',function(data){
 	    	return this;
 	  	}
 	}); 
-	var CurrentSong=Backbone.Model.extend({
-		defaults:{
-			current:0,
-			random:false,
-			repeat:true,
-			singleRepeat:false,
-		},
-		initialize:function(){
-			lib.models[this.attributes.current].play();
-		},
-		prev:function(){
-			var current=this.attributes.current;
-			var target=current-1;
-			if (target<0&&this.attributes.repeat){
-				target=lib.length-1;
-			}
-			if (this.attributes.singleRepeat) {
-				target=current;
-			}
-			return target;
-		},
-		next:function(){
-			var current=this.attributes.current;
-			var target=current+1;
-			if (target>=lib.length&&this.attributes.repeat){
-				target=0
-			}
-			if (this.attributes.singleRepeat) {
-				target=current;
-			}
-			return target;
-		},
-	});
-	var currentSong=new CurrentSong();
-	$('#play .next').on('click',function(){
-		var number=currentSong.next();
-		currentSong=new CurrentSong({current:number});
-	});
-	$('#play .prev').on('click',function(){
-		var number=currentSong.prev();
-		currentSong=new CurrentSong({current:number});
-	});
-	$('#play .play').on('click',function(){
-		var $this=$(this);
-		$this.toggleClass('paused');
-		if ($this.hasClass('paused')){
-			$player[0].pause();
-		} else {
-			$player[0].play();
-		}
-	});
-	$player.on('ended',function(event){
-		var number=currentSong.next();
-		currentSong=new CurrentSong({current:number});
-	});
 	var PlayList=Backbone.View.extend({
 		tagName:'ul',
 		className:'playlist',
@@ -98,4 +52,71 @@ $.getJSON('/getsongs',function(data){
 		}
 	});
 	var playList=new PlayList({collection:lib});
+	//song playing model
+	var CurrentSong=Backbone.Model.extend({
+		defaults:{
+			current:0,
+			random:false,
+			repeat:true,
+			singleRepeat:false,
+		},
+		initialize:function(){
+			var model=lib.models[this.attributes.current]
+			model.play();
+			model.updateInfo();
+		},
+		prev:function(){
+			var current=this.get('current');
+			var target=current-1;
+			if (target<0&&this.attributes.repeat){
+				target=lib.length-1;
+			}
+			if (this.attributes.singleRepeat) {
+				target=current;
+			}
+			return target;
+		},
+		next:function(){
+			var current=this.get('current');
+			var target=current+1;
+			if (target>=lib.length&&this.attributes.repeat){
+				target=0
+			}
+			if (this.attributes.singleRepeat) {
+				target=current;
+			}
+			return target;
+		},
+	});
+	var currentSong=new CurrentSong();
+	$('#play .next').on('click',function(){
+		var number=currentSong.next();
+		currentSong.set('current',number);
+	});
+	$('#play .prev').on('click',function(){
+		var number=currentSong.prev();
+		currentSong.set('current',number);
+	});
+	$('#play .play').on('click',function(){
+		var $this=$(this);
+		$this.toggleClass('paused');
+		if ($this.hasClass('paused')){
+			player.pause();
+		} else {
+			player.play();
+		}
+	});
+	currentSong.on('change',function(){
+		var current=this.get('current');
+		var model=lib.models[current];
+		model.play();
+		model.updateInfo();
+	},currentSong);
+	streamer.collection=lib;
+	streamer.current=currentSong;
+	player.addEventListener('ended',function(event){
+		var number=currentSong.next();
+		currentSong.set('current',number);
+	});
+	
 });
